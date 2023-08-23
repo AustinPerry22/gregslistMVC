5510e35601c8121f2b51075e20133a70e83f91cb
AustinPerry22 <austinjperry22@gmail.com>
2023-08-23 16:38:07 -0600

    local storage works

--- app/services/HousesService.js
+++ app/services/HousesService.js
@@ -1,16 +1,23 @@
 import { AppState } from "../AppState.js";
 import { House } from "../models/House.js";
+import { saveState } from "../utils/Store.js";
+
+function _saveHouses() {
+    saveState('houses', AppState.houses)
+}
 
 class HousesService {
     deleteHouse(id) {
         let filteredHouses = AppState.houses.filter(house => house.id != id)
         AppState.houses = filteredHouses
+        _saveHouses()
 
     }
     createHouse(formData) {
         let newHouse = new House(formData)
         AppState.houses.push(newHouse)
         AppState.emit('houses')
+        _saveHouses()
     }
 
 
